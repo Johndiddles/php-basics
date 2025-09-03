0809990c51de10103c6f6a1f20efe4207ff5b469
johndiddles <johnadepelumi@gmail.com>
2025-09-03 10:39:06 +0100

    insert data into mysql from php

--- mysql/index.php
+++ mysql/index.php
@@ -1,5 +1,7 @@
 <?php 
     include "database.php";
+
+    
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +12,30 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Hello</p>
+    <form action="index.php" method="post">
+        Name: <input type="text" name="name" placeholder="name" />
+        <br />
+        Password: <input type="password" name="password" placeholder="password" />
+        <br />
+        <input type="submit" value="submit" name="submit" />
+    </form>
 </body>
 </html>
+
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+        echo "$name . $password" . "<br>";
+
+        $sql = "INSERT INTO users (user, password) VALUES ('$name', '$password')";
+        try {
+           mysqli_query($conn, $sql);
+           echo "Created user successfully";
+        } catch (mysqli_sql_exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        
+    }
+    mysqli_close($conn)
+?>
