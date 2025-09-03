07dfca79c301e66212a9fa2b581c84f13d541b47
johndiddles <johnadepelumi@gmail.com>
2025-09-03 11:12:54 +0100

    read data from mysql in php

---
+++ mysql/read.php
@@ -0,0 +1,18 @@
+<?php 
+    include "database.php";
+    
+    $sql = "SELECT * FROM users WHERE user = 'John'";
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo $row["user"] . "<br>"; 
+        echo $row["id"] . "<br>";
+        echo $row["reg_date"] . "<br>";
+    } else {
+        echo "No user found";
+    };
+
+    mysqli_close($conn)
+?>
+
